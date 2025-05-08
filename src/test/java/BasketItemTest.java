edb3ea4b9df21e7f44f8779dceba0eb1cc6cc0e7
DawidKapka <kapkadawid6@gmail.com>
2025-05-08 11:18:07 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/BasketItemTest.java

--- src/test/java/BasketItemTest.java
+++ src/test/java/BasketItemTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 public class BasketItemTest {
+
+    @Test
+    void addArticleTest() {
+        ShoppingBasket basket = new ShoppingBasket();
+        Article article = new Article();
+
+        basket.addArticle(article, 1);
+
+        assertEquals(BigDecimal.ZERO, basket.getPrice());
+    }
 }
